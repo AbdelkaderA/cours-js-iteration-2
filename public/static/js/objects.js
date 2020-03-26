0a080e7789537328901cb3a7aedf386b48a8ce69
AbdelkaderA <58740908+AbdelkaderA@users.noreply.github.com>
2020-03-26 10:49:39 +0100

    Modif

--- public/static/js/objects.js
+++ public/static/js/objects.js
@@ -9,4 +9,4 @@ function load_components(){
     console.log("Chargement des données de la page");
     // Ajouter ici le code permettant de charger dynamiquement les éléments de la page
 }
-
+load_components();
